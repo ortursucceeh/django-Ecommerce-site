2ab037dc47668f271c24815ae61f70b7544dfd5c
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-03-11 23:17:35 +0200

    add category admin-setting display

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -124,6 +124,10 @@ STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [
     "mysite/static",
 ]
+
+# media files configurations 
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
