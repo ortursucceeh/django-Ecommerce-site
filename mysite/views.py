b40e94683cb458a39d2f76db3590144ca17ec3e0
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-03-17 21:52:16 +0200

    add product display on home page

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from store.models import Product
 
 def home(request):
-    return render(request, "mysite/home.html")
+    products = Product.objects.all().filter(is_avaliable=True)
+    context = {
+        "products": products
+    }
+    return render(request, "mysite/home.html", context)
