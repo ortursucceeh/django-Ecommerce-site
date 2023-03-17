22141e30372bf191d23c3f68a8366b085e217155
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-03-17 22:20:22 +0200

    add product display by category on category page

--- store/views.py
+++ store/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from category.models import Category
 # Create your views here.
-def store(request):
-    products = Product.objects.all().filter(is_avaliable=True)
-    product_count = products.count()
+def store(request, category_slug=None):
+    categories = None
+    products = None
+    
+    if category_slug:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.all().filter(category=categories, is_avaliable=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_avaliable=True)
+        product_count = products.count()
+        
     context = {
         "products": products,
         "product_count": product_count
