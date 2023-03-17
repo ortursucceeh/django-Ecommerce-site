0d38b5ef229f46a0db7b5768a0da93a54b289e3e
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-03-17 22:30:04 +0200

    add displaying categories in dropdown menu in navbar

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "category.contest_processors.menu_links",
             ],
         },
     },
