22aa16c37d86ec987b4970c0fe2dff983bdcbe1a
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-03-21 22:36:39 +0200

    add register functional

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,9 +5,11 @@ class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         "placeholder": "Enter Password"
     }))
+    
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
         "placeholder": "Confirm Password"
     }))
+    
     class Meta:
         model = Account
         fields = ["first_name", "last_name", "phone_number", "email", "password"]
@@ -20,3 +22,13 @@ class RegistrationForm(forms.ModelForm):
         self.fields["phone_number"].widget.attrs["placeholder"] = "Enter Phone Number"
         for field in self.fields:
             self.fields[field].widget.attrs["class"] = "form-control"
+            
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password") 
+        
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match!"
+            )
