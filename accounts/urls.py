7d4240e222df2a34b3fc7cc28a1be37b99451da6
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-03-22 22:03:48 +0200

    add reset password functional)

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("", views.dashboard, name="dashboard"),
     path("activate/<uidb64>/<token>", views.activate, name="activate"),
+    path("forgotPassword/", views.forgotPassword, name="forgotPassword"),
+    path("resetPassword_validate/<uidb64>/<token>", views.resetPassword_validate, name="resetPassword_validate"),
+    path("resetPassword/", views.resetPassword, name="resetPassword"),
 ]
