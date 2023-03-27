4dfd1171231b6555beba4f2c3643e1590b527f62
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-03-27 23:01:38 +0300

    add storing payment after purchase

--- orders/views.py
+++ orders/views.py
@@ -4,13 +4,26 @@ from django.shortcuts import redirect, render
 
 from carts.models import CartItem
 from orders.forms import OrderForm
-from orders.models import Order
+from orders.models import Order, Payment
 
 
 # Create your views here.
 def payments(request):
     body = json.loads(request.body)
-    print(body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body["orderID"])
+    
+    payment = Payment(
+        user = request.user,
+        payment_id = body["transID"],
+        payment_method = body["payment_method"],
+        amount_paid = order.order_total,
+        status = body["status"],
+    )
+    payment.save()
+    
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
     return render(request, "orders/payments.html")
 
 def place_order(request, total=0, quantity=0, tax=0):
