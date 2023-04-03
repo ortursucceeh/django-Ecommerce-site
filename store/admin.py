20bca9d4fd1bd79aed3bd1e4020ee6f5ae9b5654
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-04-03 21:38:25 +0300

    add reviewrating model

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Variation
+from .models import Product, Variation, ReviewRating
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
@@ -12,5 +12,7 @@ class VariationAdmin(admin.ModelAdmin):
     list_editable = ("is_active",)
     list_filter = ("product", "category", "value")
     
+    
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
+admin.site.register(ReviewRating)
