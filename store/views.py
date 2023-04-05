cbe6290cc749abc06457a7110b21034d90b4fcd3
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-04-05 23:03:43 +0300

    add avg and count of rate

--- store/views.py
+++ store/views.py
@@ -41,11 +41,14 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
     
-    try:
-        order_product = OrderProduct.objects.filter(user=request.user, product_id=product.id).exists()
-    except OrderProduct.DoesNotExist:
+    if request.user.is_authenticated:
+        try:
+            order_product = OrderProduct.objects.filter(user=request.user, product_id=product.id).exists()
+        except OrderProduct.DoesNotExist:
+            order_product = None
+    else:
         order_product = None
-    
+        
     
     # get reviews
     
